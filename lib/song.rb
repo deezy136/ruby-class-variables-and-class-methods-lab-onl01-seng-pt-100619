74c0ddcad16bbfc60da9eb5c43a5e873651c753e
deezy13 <deezy13@gmail.com>
2019-10-25 02:45:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
   attr_accessor :name, :artist, :genre
  @@count = 0
  @@artists = []
- @@genre 
- @@genre 
+ @@genre << genre 
+
  
   def initialize(name, artist, genre)
     @name = name 
