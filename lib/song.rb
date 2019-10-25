f2e1aa2c9743acbce019a38eb427eb4defb830a4
deezy13 <deezy13@gmail.com>
2019-10-25 02:58:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,17 +26,14 @@ class Song
    @@genres.uniq
  end 
   
-  def self.genre_count
+    def self.genre_count
     genre_count = {}
-    
-    @@genres.each do | genre |
-      if genre_count[genre] 
-        genre_count[genre]  = genre_count[genre] + 1 
-        
-      else 
-        genre_count[genre] = 1 
-    end 
-  end 
-end 
-  genre_count
-end 
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
