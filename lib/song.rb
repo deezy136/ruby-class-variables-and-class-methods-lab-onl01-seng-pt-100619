0bd9eb63bd382a22ec300d963872d21d36d1cabf
deezy13 <deezy13@gmail.com>
2019-10-25 02:22:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+ 
+ 
+  def self.initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+    
+  end 
+ 
+ def self.count
+   @@count+=1
+ end 
+ 
+ def self.genre
+   if !(GENRES.include?(@genre))
+    BRANDS.push(@brand)
+ end
+ end 
+  
+end 
